bf5263934fff38ecfebfa65fb0d3d13a04086118
Bing Xie <bing708@gmail.com>
2015-09-16 14:06:11 +1000

    Add todo header and initiate the firebase

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,27 @@
 var React = require('react');
 var ReactFire = require('reactfire');
 var Firebase = require('firebase');
+var Header = require('./header')
+
+var rootUrl = 'https://luminous-heat-9884.firebaseio.com/';
 
 var App = React.createClass({
+  mixins: [ ReactFire], // minxins把相应地对象的方法copy过来
+
+  componentWillMount: function() {
+    // bindAsObject是reactfire提供的方法
+    this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+  },
+
   render: function() {
-    return <h1 className="red">
-      Hello, React!
-    </h1>
+    return <div className="row panel panel-default">
+      <div className="col-md-8 col-md-offset-2">
+        <h2 className="text-center">
+          To-Do List
+        </h2>
+        <Header />
+      </div>
+    </div>
   }
 });
 
